c49e0e444bc4339183e5ce36c8e806ff6c3496c7
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-26 14:54:35 -0300

    <Header /> Criando componente

---
+++ layout/Header/index.tsx
@@ -0,0 +1,9 @@
+function Header() {
+	return (
+		<header className='mb-10 p-2 text-center border-b bg-slate-100'>
+			<h1 className='text-sm'>Conteúdo sobre Front-end por <span className='font-bold'>Matheus Pergoli</span>.</h1>
+		</header>
+	)
+}
+
+export default Header
