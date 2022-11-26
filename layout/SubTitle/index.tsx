8b1f6596d049ed0b0616695d35e957c85ed59380
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-25 21:58:21 -0300

    <SubTitle /> Mudando fonte

--- layout/SubTitle/index.tsx
+++ layout/SubTitle/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function SubTitle(props: { children: React.ReactNode }) {
-	return <h2 className='text-center text-4xl font-bold mb-10 border-t pt-10 sm:text-left sm:text-5xl'>{props.children}</h2>
+	return <h2 className='text-center text-5xl font-bold mb-10 border-t pt-10 sm:text-left'>{props.children}</h2>
 }
 
 export default SubTitle
