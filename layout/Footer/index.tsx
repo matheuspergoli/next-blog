14b3def6f5f2da239368287195e873e24c094b24
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-26 04:43:10 -0300

    <Footer /> Modificando classes

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
-		<footer className='flex flex-col items-center justify-around h-72 mt-40 lg:flex-row bg-footer-color text-white'>
-			<p className='text-4xl font-bold text-center sm:text-left'>Blog feito por Matheus Pergoli.</p>
+		<footer className='flex flex-col items-center justify-around h-60 mt-20 lg:flex-row bg-footer-color text-white'>
+			<p className='text-3xl font-bold text-center sm:text-left'>Blog feito por Matheus Pergoli.</p>
 			<div className='flex gap-5'>
 				<a href='https://matheuspergoli.vercel.app' target='_blank' rel='noreferrer' className='text-2xl underline'>
 					Portfólio
