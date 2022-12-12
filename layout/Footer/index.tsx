5fbaa32346c34c5adcdff1e7c6073094c569b8d6
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-12 17:07:52 -0300

    refactor: add icons for links

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,28 +1,26 @@
+import { FaGithub as GithubIcon, FaLinkedin as LinkedInIcon, FaUserCircle as UserIcon } from 'react-icons/fa'
+
 function Footer() {
 	return (
 		<footer className='mt-20 flex h-60 flex-col items-center justify-around bg-footer-color text-white lg:flex-row'>
 			<p className='text-center text-3xl font-bold sm:text-left'>Blog feito por Matheus Pergoli.</p>
-			<div className='flex gap-5'>
+			<div className='flex items-center gap-5'>
 				<a
 					href='https://matheuspergoli.vercel.app'
 					target='_blank'
 					rel='noreferrer'
-					className='text-2xl underline'>
-					Portfólio
+					className='text-4xl underline'>
+					<UserIcon title='Portfólio' className='transition hover:scale-110' />
 				</a>
 				<a
 					href='https://www.linkedin.com/in/matheuspergoli/'
 					target='_blank'
 					rel='noreferrer'
-					className='text-2xl underline'>
-					LinkedIn
+					className='text-4xl underline'>
+					<LinkedInIcon title='LinkedIn' className='transition hover:scale-110' />
 				</a>
-				<a
-					href='https://github.com/matheuspergoli'
-					target='_blank'
-					rel='noreferrer'
-					className='text-2xl underline'>
-					Github
+				<a href='https://github.com/matheuspergoli' target='_blank' rel='noreferrer' className='text-4xl'>
+					<GithubIcon title='Github' className='transition hover:scale-110' />
 				</a>
 			</div>
 		</footer>
