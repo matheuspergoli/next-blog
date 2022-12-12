95a54f078e7feadc18b61afce8e256ea3cc7abd3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-12 16:02:48 -0300

    refactor(maincontainer/maintitle): modify the style classes

--- components/HeroPost/index.tsx
+++ components/HeroPost/index.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
 
+//sm:max-h-hero-image
+
 function HeroPost(props: PreviewPost) {
 	return (
 		<article className='mb-20'>
 			<Link href={`/post/${props.slug}`}>
-				<img src={props.coverImage.url} alt='Post cover' className='max-h-hero-image object-cover w-full mb-5' />
+				<img
+					src={props.coverImage.url}
+					alt='Post cover'
+					className='max-h-60 w-full mb-5 object-cover md:max-h-80 lg:max-h-96 xl:max-h-hero-image'
+				/>
 			</Link>
 			<div className='grid grid-cols-1 px-3 gap-5 sm:p-0 lg:grid-cols-2 lg:gap-10'>
 				<Link href={`/post/${props.slug}`} className='transition hover:underline'>
@@ -13,7 +19,11 @@ function HeroPost(props: PreviewPost) {
 				<section>
 					<p className='text-2xl mb-5'>{props.excerpt}</p>
 					<div className='flex items-center gap-4'>
-						<img src={props.author.picture.url} alt='Author image' className='w-12 h-12 rounded-full' />
+						<img
+							src={props.author.picture.url}
+							alt='Author image'
+							className='w-12 h-12 rounded-full'
+						/>
 						<p className='text-lg font-bold'>{props.author.name}</p>
 					</div>
 				</section>
--- layout/MainTitle/index.tsx
+++ layout/MainTitle/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function MainTitle(props: { children: React.ReactNode }) {
-	return <h1 className='text-5xl font-bold mb-10'>{props.children}</h1>
+	return (
+		<h1 className='text-center text-5xl font-bold mb-10 sm:text-left sm:text-7xl lg:text-8xl'>
+			{props.children}
+		</h1>
+	)
 }
 
 export default MainTitle
